2f924f6085a11c03775a55b01f84510c6017bcea
Manu Krishna <krishnamanu27112004@gmail.com>
2023-11-19 19:38:40 +0530

    Update DS-2.c

--- DS-2.c
+++ DS-2.c
@@ -1,1 +1,36 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i, j, rows, coloumns, srchelement, count=0;
+	int array[100][100];
+	clrscr();
+	printf("Enter number of rows:");
+	scanf("%d", &rows);
+	printf("\nEnter number of colouns:");
+	scanf("%d", &coloumns);
+	printf("Enter %d elements:\n", (rows*coloumns));
+	for(i=0; i<rows; i++)
+	{
+		for(j=0; j<coloumns; j++)
+		{
+			scanf("%d", &array[i][j]);
+		}
+	}
+	printf("Enter the elements to get the position:\n");
+	scanf("%d", &srchelement);
+	for(i=0; i<rows; i++)
+	{
+		for(j=0; j<coloumns; j++)
+		{
+			if(array[i][j]==srchelement)
+			{
+				printf("(%d,%d)\n", i, j);
+				count++;
+			}
+		}
+	}
+	if(count==0)
+	printf("Not found\n");
+	getch();
+}
