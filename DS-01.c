1d9d9cc3b9da568c90c9ec4580a2ed3187331d59
Manu Krishna <krishnamanu27112004@gmail.com>
2023-12-09 19:20:23 +0530

    Update DS-01.c

--- DS-01.c
+++ DS-01.c
@@ -4,26 +4,26 @@
 #include<string.h>
 void main()
 {
-    int l, i;
-    char str[100], *begin_ptr, *end_ptr, ch;
-    clrscr();
-    printf("Enter a string:\n");
-    scanf("%s", str);
-    l=strlen(str);
-    begin_ptr=str;
-    end_ptr=str;
-    for(i=0; i<l-1; i++)
-    {
-        end_ptr++;
-    }
-    for(i=0; i<l/2; i++)
-    {
-        ch=*end_ptr;
-        *end_ptr=*begin_ptr;
-        *begin_ptr=ch;
-        begin_ptr++;
-        end_ptr--;
-    }
-    printf("Reverse of the string: %s\n", str);
-    getch();
+	int l, i;
+	char str[100], *begin_ptr, *end_ptr, ch;
+	clrscr();
+	printf("Enter a string:\n");
+	scanf("%s", str);
+	l=strlen(str);
+	begin_ptr=str;
+	end_ptr=str;
+	for(i=0; i<l-1; i++)
+	{
+		end_ptr++;
+	}
+	for(i=0; i<l/2; i++)
+	{
+		ch=*end_ptr;
+		*end_ptr=*begin_ptr;
+		*begin_ptr=ch;
+		begin_ptr++;
+		end_ptr--;
+	}
+	printf("Reverse of the string: %s\n", str);
+	getch();
 }
