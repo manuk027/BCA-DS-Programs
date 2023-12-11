1c172e277ea4bec65aa5c8c672e9e00dcc27e182
Manu Krishna <krishnamanu27112004@gmail.com>
2023-12-11 18:48:09 +0530

    Create DS-02.c

---
+++ DS-02.c
@@ -0,0 +1,27 @@
+//pattern matching
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+void main()
+{
+	char text[20], pat[20];
+	int a, b, i, j;
+	clrscr();
+	printf("Enter the string:");
+	gets(text);
+	printf("Enter the pattern to find:");
+	gets(pat);
+	a=strlen(pat);
+	b=strlen(text);
+	for(i=0; i<b-a; i++)
+	{
+		int j;
+		for(j=0; j<a; j++)
+		if(text[i+j]!=pat[j])
+		break;
+		if(j==a)
+		printf("Pattern found at position %d\n", i+1);
+	}
+	getch();
+}
