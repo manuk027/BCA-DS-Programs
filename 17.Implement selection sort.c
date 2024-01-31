44921b8d743c7aaea0e4e0bca158cdf7f31d17a4
Manu Krishna <krishnamanu27112004@gmail.com>
2024-01-31 15:09:01 +0530

    Create 17.Implement selection sort.c

---
+++ 17.Implement selection sort.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h> 
+void main()
+{
+int array[100],n,c,d,position,swap;
+printf("\n Enter no of elements:");
+scanf("%d",&n);
+printf("\n Enter %d integers: \n",n); 
+for(c=0;c<n;c++)
+scanf("%d",&array[c]);
+for(c=0;c<(n-1);c++)
+{
+position=c;
+for(d=c+1;d<n;d++)
+{
+if(array[position]>array[d])
+position=d;
+}
+if(position!=c)
+{
+swap=array[c];
+array[c]=array[position];
+array[position]=swap;
+}
+}
+printf("\n Sorted list in ascending order:\n");
+for(c=0;c<n;c++)
+printf("%d\n",array[c]); 
+getch();
+}
